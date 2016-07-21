984bb93128051f2ed23eb644768d6b64d6d264e9
openerp7 <openerp7@panopenerp.micronaet.local>
2016-07-21 10:22:24 +0200

    End order export procedure

--- xmlrpc_operation_order/operation.py
+++ xmlrpc_operation_order/operation.py
@@ -81,57 +81,31 @@ class SaleOrder(orm.Model):
         ''' Export current order 
             # TODO manage list of order?
         '''
-        def clean_description(value):
-            ''' Remove \n and \t and return first 40 char
-            ''' 
-            value = value.replace('\n', ' ')            
-            value = value.replace('\t', ' ') 
-            return value[:40]
-            
+        def format_date(value):
+            ''' Set date for accounting program:
+            '''
+            if value:
+                return '%s%s%s' % (
+                    value[:4], 
+                    value[5:7], 
+                    value[8:10], 
+                    )
+            return ''        
+                
         assert len(ids) == 1, 'No multi export for now' # TODO remove!!!
 
         # TODO use with validate trigger for get the number
         parameter = {}
         
-        # Generate string for export file:
-        ''' 
-        NUMERO                    6
-        CAUSALE                    2
-        DATA                          8
-        COD.CLIENTE            9
-        SCADENZA                8
-        COD.AGENTE            9
-        NOTE                        16
-        
-        corpo righe        
-        CODICE ARTICOLO   8
-        DESCR.ARTICOLO    60
-        UM                              2
-        QUANTITA'              5+3 dec
-        PREZZO                    5+5 dec
-        SCONTO                   20
-        IVA o ESENZIONI       5
-        
-        piede del documento
-        TOT COLLI                  5
-        PESO TOT KG            15
-        PORTO                        3
-        TRASP.MEZZO           1 (V vettore M mittente D dest)
-        '''
         mask = '%s%s%s%s' % ( #3 block for readability:
             '%-6s%-2s%-8s%-9s%-8s%-9s%-16s', #header
-            '%-8s%-60s%-2s%9.3f%11.5f%-20s%-5s%', #row
+            '%-8s%-60s%-2s%8s%10s%-20s%-5s%-8s', #row
             '%-5s%-15s%-3s%1s', #foot
             '\r\n', # Win CR
             )
 
         parameter['input_file_string'] = ''
         for order in self.browse(cr, uid, ids, context=context):
-            if not order.number:
-                raise osv.except_osv(
-                    _('XMLRPC sync error'), 
-                    _('order must be validated!'))
-                
             for line in order.order_line:
                 parameter['input_file_string'] += self.pool.get(
                     'xmlrpc.server').clean_as_ascii(
@@ -139,59 +113,36 @@ class SaleOrder(orm.Model):
                             # -------------------------------------------------
                             #                    Header:
                             # -------------------------------------------------
-                            order.name,
-                            order.causal,
-                            '%s%s%s' % (
-                                order.date_order[:4], 
-                                order.date_order[5:7], 
-                                order.date_order[8:10], 
-                                ),
-                            order.partner_id.sql_customer_code,
-                            order.deadline,
-                            order.mx_agent_id.sql_agent_code or \
-                                order.mx_agent_id.sql_supplier_code or '',
-                            order.note,
-                            
+                            order.name.split('/')[0][:6],
+                            '',# TODO order.causal,
+                            format_date(order.date_order),
+                            order.partner_id.sql_customer_code or '',
+                            format_date(order.date_deadline),
+                            '', # TODO Agent code  
+                            order.note[:16] if order.note else '',
+                                                        
                             # -------------------------------------------------
                             #                    Lines:
                             # -------------------------------------------------
-
-                            # -------------------------------------------------
-                            #                    Detail:
-                            # -------------------------------------------------
-                            line.product_id.default_code or '', 
-                            clean_description(
-                                line.name if line.use_text_description \
-                                    else line.product_id.name),
-                            # UOM (2)
-                            line.product_id.uom_id.account_ref or '',
-                            # Q. 10N (2 dec.)
-                            line.quantity, 
-                            # Price 10N (3 dec.)
-                            line.price_unit, 
-                            # Tax (5)
-                            line.order_line_tax_id[0].account_ref \
-                                if line.order_line_tax_id else '', 
-                            # Provv. (5)
-                            0, 
-                            # Discount (50)
-                            line.multi_discount_rates or '',
-                            # Discount numeric (10)
+                            (line.product_id.default_code or '')[:8],
+                            ((line.name.split('] ')[-1]).split('\n')[0])[:60],
+                            '', # TODO line.product_id.uom_id.account_ref or ''
+                            ('%9.3f' % line.product_uom_qty).replace('.', ''),
+                            ('%11.5f' % line.price_unit).replace('.', ''),
                             line.discount or '',
-                            # Account (8)
-                            line.account_id.account_ref or '', 
-                            # Refund (1)
-                            refund_line,
+                            '', # TODO vat tax_id
+                            format_date(line.date_deadline),
 
                             # -------------------------------------------------
-                            #                     Foot:
+                            #                    Footer:
                             # -------------------------------------------------
-                            # Codice Pagamento 3
-                            order.payment_term.import_id \
-                                if order.payment_term else '', 
-                            # TODO bank
+                            '', # TODO total parcels 
+                            '', # TODO weight total
+                            '', # TODO port
+                            '', # TODO transport
                             ))
 
+        import pdb; pdb.set_trace()
         res = self.pool.get('xmlrpc.operation').execute_operation(
             cr, uid, 'order', parameter=parameter, context=context)
             
