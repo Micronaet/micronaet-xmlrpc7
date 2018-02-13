46b10955657a5dbe0a2b1afb0ceda8be6a38cfc3
nicothebrush <nicola.riolini@gmail.com>
2018-02-13 09:56:05 +0100

    generic bugfix (fast message)

--- xmlrpc_operation_lot_create/operation.py
+++ xmlrpc_operation_lot_create/operation.py
@@ -27,7 +27,7 @@ import openerp.addons.decimal_precision as dp
 from openerp.osv import fields, osv, expression, orm
 from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
-from openerp import SUPERUSER_ID, api
+from openerp import SUPERUSER_ID
 from openerp import tools
 from openerp.tools.translate import _
 from openerp.tools.float_utils import float_round as round
@@ -80,7 +80,7 @@ class MrpProduction(orm.Model):
     # -------------------------------------------------------------------------
     # Scheduled
     # -------------------------------------------------------------------------
-    def xmlrpc_export_lot_create(self, cr, uid, ids, from_date, context=None):
+    def xmlrpc_export_lot_create(self, cr, uid, from_date, context=None):
         ''' Schedule update of production on accounting
         '''
         # Utility:
